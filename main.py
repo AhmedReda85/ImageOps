71d6653c2ad8ba2d5629d784e4fc9b087d544240
Ahmed Reda <AhmedRedaSayed_sd23@fcis.bsu.edu.eg>
2024-11-09 02:35:16 +0200

    Add Filters

--- main.py
+++ main.py
@@ -7,47 +7,86 @@ def input_value():
     value=int(input())
     return value
 
-def more_bright(value):
+def more_bright():
     print("Enter a value")
-    input_value(value)
+    value=input_value()
     image=np.copy(img)
     h,w=np.shape(image)
-    for col in range(w):
-        for row in range(h):
-            image[col][row]=image[col][row]*value
-    cv2.imshow("Bright Image",image)
-    
+    for row in range(h):
+        for col in range(w):
+            image[row][col]=image[row][col]*value
+    return image    
 
-def more_dark(value):
+def more_dark():
     print("Enter a value")
-    input_value(value)
+    value=input_value()
     image=np.copy(img)
     h,w=np.shape(image)
-    for col in range(w):
-        for row in range(h):
-            image[col][row]=image[col][row]/value
-    cv2.imshow("Dark Image",image)
-    
+    for row in range(h):
+        for col in range(w):
+            image[row][col]=image[row][col]/value
+    return image    
 def negative():
     image=np.copy(img)
     h,w=np.shape(image)
-    for col in range(w):
-        for row in range(h):
-            image[col][row]=1-image[col][row]
-    cv2.imshow("Image",image)
-def power_law(value):
-    gamma_value=input_value(value)
+    for row in range(h):
+        for col in range(w):
+            image[row][col]=1-image[row][col]
+    return image    
+def power_law():
+    gamma_value=input_value()
     image=np.copy(img)
     h,w=np.shape(img)
-    for col in range(w):
-        for row in range(h):
-            image[col][row]=(2*image[col][row])**gamma_value
-    cv2.imshow("Image",image)
+    for row in range(h):
+        for col in range(w):
+            image[row][col]=(2*image[row][col])**gamma_value
+    return image
+def log_transformation():
+    scaling_factor_value=input_value()
+    image=np.copy(img)
+    h,w=np.shape(image)
+    for row in range(h):
+        for col in range(w):
+            if image[row][col] > 0:
+                image[row][col]=scaling_factor_value*(np.log2(image[row][col]))
+            else:
+                image[row][col]=0
+    return image    
 
+def average_filter():
+    image=np.copy(img)
+    h,w=np.shape(image)
+    for row in range(1,h-1):
+        for col in range(1,w-1):
+            image[row][col]=np.mean(image[row-1:row+2, col-1:col+2])
+    return image
+    
+def max_filter():
+    image=np.copy(img)
+    output_image=np.zeros_like(image)
+    h,w=np.shape(image)
+    for row in range(1,h-1):
+        for col in range(1,w-1):
+            neighbors=image[row-1:row+2, col-1:col+2]
+            output_image[row][col]=np.max(neighbors)
+    return output_image
 
+def min_filter():
+    image=np.copy(img)
+    output_image=np.zeros_like(image)
+    h,w=np.shape(image)
+    for row in range(1,h-1):
+        for col in range(1,w-1):
+            neighbors=image[row-1:row+2, col-1:col+2]
+            output_image[row][col]=np.min(neighbors)
+    return output_image
 
-# cv2.imshow("Log Transformation", log_img)
-# cv2.imshow('Original', original)
-# cv2.imshow('Negative', img)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
+def median_filter():
+    image=np.copy(img)
+    output_image=np.zeros_like(image)
+    h,w=np.shape(image)
+    for row in range(1,h-1):
+        for col in range(1,w-1):
+            neighbors=image[row-1:row+2, col-1:col+2]
+            output_image[row][col]=np.median(neighbors)
+    return output_image
